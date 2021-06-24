f41324434d26641cce347e52a1a3d01741d3f7fa
Arthur Anderson <arthur.anderson3@gmail.com>
2021-06-24 14:20:54 -0600

    moved dist to docs for github pages

--- webpack.common.js
+++ webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].[contenthash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   optimization: {
